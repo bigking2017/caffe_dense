ffa2ff4e76a7e1fa7604a6ee8e75736bac458e76
kingk <>
2017-11-16 01:34:12 +0000

    first commit

---
+++ scripts/create_dense_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./build/tools/convert_dense_data.bin --label_file=/home/kingk/face_sample/face_01/feature.txt /home/kingk/face_sample/face_01/ /home/kingk/face_work/caffe-centerloss/data/face_fit/lmdb
